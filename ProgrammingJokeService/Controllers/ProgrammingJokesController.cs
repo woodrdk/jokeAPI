b7abb1d4f9071593bd09ae28ccac252a9f4f092f
Rob Wood <rdrwood@gmail.com>
2018-11-26 15:44:29 -0800

    can delete from database and partially update.

--- ProgrammingJokeService/Controllers/ProgrammingJokesController.cs
+++ ProgrammingJokeService/Controllers/ProgrammingJokesController.cs
@@ -23,23 +23,67 @@ namespace ProgrammingJokeService.Controllers
         [HttpGet]
         public async Task<IActionResult> GetJoke()
         {
-            List<Joke> jokes = await _db.Jokes.ToListAsync();
+            List<Joke> jokes = await JokeDb.GetAllJokes(_db);
 
             return Ok(jokes);
         }
 
+      
+
         // mywebsite.com/api/ProgrammingJokes/category/C#
         [HttpGet("category/{category}")]
-        public async Task<IActionResult> GetJokesByCategory([FromRoute]string category)
+        public IActionResult GetJokesByCategory([FromRoute]string category)
         {
-            List<Joke> jokesCat = (from j in _db.Jokes
-                                   where j.Category == category
-                                   select j).ToList();
-
-
-               //await _db.Jokes.ToListAsync();
+            List<Joke> jokesCat = JokeDb.JokesByCategory(category, _db);
 
             return Ok(jokesCat);
         }
+
+        // to add joke to db
+        [HttpPost]
+        public IActionResult PostJoke([FromBody] Joke j)
+        {
+            if (ModelState.IsValid)
+            {
+                JokeDb.addJoke(_db, j);
+                return Ok();
+            }
+            else
+            {
+                // return http 400, with modelstate errors
+                return BadRequest(ModelState); //throw new Exception("test");
+            }
+        }
+
+        // mywebsite.com/api/programmingjokes/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteJoke([FromRoute] int id)
+        {
+            if (JokeDb.DoesExist(_db, id))
+            {
+                JokeDb.DeleteJoke(_db, id);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // mywebsite.com/api/programmingjokes/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateOldJoke([FromRoute] int id, string jokeText, string jokeCategory)
+        {
+            if (JokeDb.DoesExist(_db, id))
+            {
+                JokeDb.UpdateJoke(_db, id, jokeText, jokeCategory);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
     }
 }
---
+++ ProgrammingJokeService/Models/JokeDb.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProgrammingJokeService.Models
+{   
+    /// <summary>
+    /// CRUD Functionality for Jokes
+    /// </summary>
+    public static class JokeDb
+    {
+        /// <summary>
+        /// Retrieves all Jokes from the database.
+        /// </summary>
+        public static async Task<List<Joke>> GetAllJokes(JokeDbContext _db)
+        {
+            return await _db.Jokes.ToListAsync();
+        }
+
+        /// <summary>
+        /// Get all jokes by Category
+        /// </summary>
+        /// <param name="category">What category of the joke we are looking for</param>
+        public static List<Joke> JokesByCategory(string category, JokeDbContext _db)
+        {
+            return (from j in _db.Jokes
+                    where j.Category == category
+                    select j).ToList();
+        }
+
+        public static void addJoke(JokeDbContext _db, Joke newData)
+        {
+            _db.Jokes.Add(newData);
+            _db.SaveChanges();
+        }
+
+        public static void UpdateJoke(JokeDbContext _db, int JokeId, string jokeText, string jokeCategory)
+        {
+            Joke j = _db.Jokes.Find(JokeId);
+
+            _db.Jokes.Attach(j).State = EntityState.Modified;
+            j.JokeText = jokeText;
+            j.Category = jokeCategory;
+            _db.SaveChanges();
+        }
+
+        public static void DeleteJoke(JokeDbContext _db, int jokeId)
+        {
+            Joke j = _db.Jokes.Find(jokeId); // find joke and start tracking
+             _db.Jokes.Remove(j);           // marks joke as deleted
+            _db.SaveChanges();              // saves to database
+        }
+
+        public static bool DoesExist(JokeDbContext _db, int id)
+        {
+            // returns the true if the jokeId exists in the database.
+            return _db.Jokes.Where(j => j.JokeId == id).Any();
+
+        }
+    }
+}
